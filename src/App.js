7eb5fcc62a1901de406ef30f444d11a1436f401e
Chadpaul97 <chadpaul97@gmail.com>
2022-01-17 12:26:18 -0600

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import BoxGenerator from './components/BoxGenerator';
 import './App.css';
-import { ColorDisplay } from './components/ColorDisplay';
 
 function App() {
-  const [color,setColor] = useState("No Colors")
   return (
     <div className="App">
         <BoxGenerator/>
-        <ColorDisplay color={color}></ColorDisplay>
     </div>
   );
 }
--- src/components/BoxGenerator.js
+++ src/components/BoxGenerator.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function BoxGenerator() {
 
-    const colorForm = (props) => {
-        const [color, setColor] = useState("");
 
-        const sumbitForm = (e) => {
-            e.preventDefault();
-            props.onNewColor(color)
-        }
+const BoxGenerator = (props) => {
+    const [box, setBox] = useState([]);
+    const [color, setColor] = useState("");
+
+    const sumbitForm = (e) => {
+        e.preventDefault();
+        const newColor = {
+            color: color
+        };
+        setBox([...box, newColor]);
+        setColor("");
     }
-    return (
+
+return (
+    <div>
         <div>
-            <form onSubmit={ sumbitForm }>
+            <form onSubmit={sumbitForm}>
                 <label>Color</label>
                 <input placeholder='Enter Color Here' onChange={(e) => setColor(e.target.value)} value={color}></input>
-                <button type="submit" value= "ADD">ADD</button>
+                <button type="submit" value="ADD">ADD</button>
             </form>
+            <div className='box'>
+                { box.map((box,i) => {
+                    return (<div key={i} style={{backgroundColor : box.color, height : 100, width :100}}/>)
+                } ) }
+            </div>
         </div>
-    )
+    </div>
+)
 }
+
+export default BoxGenerator
